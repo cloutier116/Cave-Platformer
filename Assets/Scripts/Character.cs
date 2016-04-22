b7470ccadc0f2b94c4d60c8dd3f7b4cbe1dd2dcc
Chris <cloutc@rpi.edu>
2016-04-22 17:46:24 -0400

    Made the thing

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,13 +3,60 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
+    float speed = .05f;
+    Rigidbody2D rb;
+    float jumpForce = 4;
+    bool onGround = false;
+
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, .2f);
+        if (hit.collider != null && (hit.transform.tag == "Ground" || hit.transform.tag == "Platform")) 
+        {
+            onGround = true;   
+        }
+        else
+        {
+            onGround = false;
+        }
+
+        float input = Input.GetAxis("Horizontal"); 
+	    if(input != 0)
+        {
+            transform.Translate(input * speed, 0, 0);
+        }
+
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            print("Jumping");
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        if (transform.position.y < -2f)
+            Reset();
 	}
+
+    void Reset()
+    {
+        transform.position = new Vector3(-15, -1, 0);
+        rb.velocity = Vector3.zero;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+            transform.parent = other.transform;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+            transform.parent = null;
+    }
 }
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Platform : MonoBehaviour {
+
+    public float trackerRange = 1.5f;
+
+    public Vector3 pos1;
+    public Vector3 pos2;
+
+    public Tracker tracker;
+
+    public bool useX, useY, useZ;
+
+    Vector3 velocity = Vector3.zero;
+    public float smoothTime = .05f;
+
+    [Range(0, 1)]
+    public float pos;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (tracker.objs.ContainsKey("Visor"))
+        {
+            if (useX)
+            {
+                pos = tracker.objs["Visor"].x;
+                pos = (pos + trackerRange) / (2 * trackerRange);
+                rb.position = Vector3.SmoothDamp(transform.position, Vector3.Lerp(pos1, pos2, pos), ref velocity, smoothTime);
+            }
+            else if (useY)
+            {
+                pos = tracker.objs["Visor"].y;
+                pos = (pos - 1)/.6f;
+                rb.position = Vector3.SmoothDamp(transform.position, Vector3.Lerp(pos1, pos2, pos), ref velocity, smoothTime);
+            }
+            else if(useZ)
+            {
+                pos = tracker.objs["Visor"].z;
+                pos = (pos + trackerRange) / (2 * trackerRange);
+                rb.position = Vector3.SmoothDamp(transform.position, Vector3.Lerp(pos1, pos2, pos), ref velocity, smoothTime);
+            }
+        }
+	}
+
+    
+}
